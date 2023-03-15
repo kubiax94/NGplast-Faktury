4691d6d640283f1b776d469fe0173f7fb3a6399f
kubiax94 <kubiax12@gmail.com>
2023-03-15 23:00:41 +0100

    Dodaj pliki projektów.

---
+++ Services/FilesService/FilesReader.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Services.FilesService
+{
+    public class FilesReaderService
+    {
+        List<string> formats = new List<string>();
+
+        public string SearchPrefix { get; set; }
+        public string SearchLocation { get; set; }
+        public List<string> AllSearchedFiles { get; }
+
+        public FilesReaderService() 
+        {
+            SearchPrefix = "";
+            SearchLocation = "";
+            AllSearchedFiles = new List<string>();
+            
+        }
+
+        public IEnumerable<string> CreateFileList()
+        {
+            List<string> allFiles = Directory.GetFiles(SearchLocation, "*.*", SearchOption.TopDirectoryOnly).ToList();
+
+            List<string> correctFiles = new List<string>();
+
+            foreach (string file in allFiles)
+            {
+                var splited = file.Split(@"\").Last().ToLower();
+                bool correct = false;
+
+                for(int i = 0; i < SearchPrefix.Length; i++) 
+                {
+                    if (SearchPrefix[i] != splited[i])
+                    {
+                        correct = false;
+                        break;
+                    }
+                    correct = true;
+                }
+
+                if (correct)
+                    correctFiles.Add(file);
+
+            }
+            return correctFiles;
+        } 
+    }
+}
---
+++ Services/OCRService/InvoiceOCR.cs
@@ -0,0 +1,43 @@
+﻿using IronOcr;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.OCRService
+{
+    public class InvoiceOCR
+    {
+        string _inputFile = "C:/test.pdf";
+        Stream _outputFile;
+        
+
+
+
+        public InvoiceOCR() {
+
+        }
+
+        public void SearchForInnvoicesDetails(IEnumerable<string> files) 
+        {
+
+            var Ocr = new IronTesseract();
+
+
+
+            foreach(var file in files)
+            {
+                using (var Input = new OcrInput(file))
+                {
+
+                    // Input.Deskew();  // use if image not straight
+                    // Input.DeNoise(); // use if image contains digital noise
+                    var Result = Ocr.Read(Input);
+
+                    Console.WriteLine(Result.Text);
+                }
+            }
+        }
+    }
+}
